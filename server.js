74468ac9b4614197e1be68ab1c076164109887cb
Yash Baid <138425939+Yashbaid19@users.noreply.github.com>
2025-07-12 14:56:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(cors({ origin: "https://skill-swap-frontend-02.vercel.app/" }));
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/swaps", swapRoutes);
